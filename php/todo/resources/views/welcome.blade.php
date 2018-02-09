fa01eb052d2652b9424b150567fa8530d949a465
Carlos Sánchez Cazorla <csanchez@bitnami.com>
2018-02-09 09:39:06 +0100

    Polishing the welcome page in the PHP To Do Application

--- php/todo/resources/views/welcome.blade.php
+++ php/todo/resources/views/welcome.blade.php
@@ -2,14 +2,11 @@
 <html>
     <head>
         <title>Laravel</title>
-
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
         <style>
             html, body {
                 height: 100%;
             }
-
             body {
                 margin: 0;
                 padding: 0;
@@ -18,27 +15,30 @@
                 font-weight: 100;
                 font-family: 'Lato';
             }
-
             .container {
                 text-align: center;
                 display: table-cell;
                 vertical-align: middle;
             }
-
             .content {
                 text-align: center;
                 display: inline-block;
             }
-
             .title {
                 font-size: 96px;
+            }
+            .subtitle {
+                margin-top:10px;
+                font-size: 48px;
+                color: #a5a5a5;
             }
         </style>
     </head>
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">My Awesome To Do App</div>
+                <div class="subtitle">using Laravel 5</div>
             </div>
         </div>
     </body>
